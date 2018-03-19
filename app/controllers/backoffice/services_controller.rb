e49435d68eb6f755a879aa6b1b4aa467626a0f27
diegoamorim <diegomaciel72.72@gmail.com>
2018-03-19 17:13:12 -0300

    adding some views and controllers

--- app/controllers/backoffice/services_controller.rb
+++ app/controllers/backoffice/services_controller.rb
@@ -1,6 +1,61 @@
 class Backoffice::ServicesController < ApplicationController
   before_action :authenticate_admin!
+  layout "backoffice"
   
   def index
+    @services = Service.all
   end
+
+  # GET /services/1
+  # GET /services/1.json
+  def show
+  end
+
+  # GET /services/new
+  def new
+    @service = Service.new
+  end
+
+  # GET /services/1/edit
+  def edit
+  end
+
+  # POST /services
+  # POST /services.json
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to backoffice_services_path, notice: "O Serviço #{@service.name} foi cadastrado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /services/1
+  # PATCH/PUT /services/1.json
+  def update
+    if @service.update(service_params)
+      redirect_to backoffice_services_path, notice: "O Serviço #{@service.name} foi atualizado com sucesso."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /services/1
+  # DELETE /services/1.json
+  def destroy
+    @service.destroy
+      redirect_to backoffice_services_path, notice: "O Serviço #{@service.name} foi deletado com sucesso."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_service
+      @service = Service.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def service_params
+      params.require(:service).permit(:name, :price)
+    end
 end
--- app/controllers/backoffice/vehicles_controller.rb
+++ app/controllers/backoffice/vehicles_controller.rb
@@ -1,6 +1,61 @@
 class Backoffice::VehiclesController < ApplicationController
   before_action :authenticate_admin!
+  layout "backoffice"
   
   def index
+    @vehicles = Vehicle.all
   end
+
+  # GET /vehicles/1
+  # GET /vehicles/1.json
+  def show
+  end
+
+  # GET /vehicles/new
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  # GET /vehicles/1/edit
+  def edit
+  end
+
+  # POST /vehicles
+  # POST /vehicles.json
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    if @vehicle.save
+      redirect_to backoffice_vehicles_path, notice: "O Veículo #{@vehicle.plate} foi cadastrado com sucesso."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /vehicles/1
+  # PATCH/PUT /vehicles/1.json
+  def update
+    if @vehicle.update(vehicle_params)
+      redirect_to backoffice_vehicles_path, notice: "O Veículo #{@vehicle.plate} foi atualizado com sucesso."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /vehicles/1
+  # DELETE /vehicles/1.json
+  def destroy
+    @vehicle.destroy
+      redirect_to backoffice_vehicles_path, notice: "O Veículo #{@vehicle.plate} foi deletado com sucesso."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_vehicle
+      @vehicle = Vehicle.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def vehicle_params
+      params.require(:vehicle).permit(:plate, :color, :model, :email, :observations)
+    end
 end
