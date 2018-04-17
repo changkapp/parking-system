31b4fe2a53f04e01b29d6dee39ba8af94cebd5d0
diegoamorim <diegomaciel72.72@gmail.com>
2018-04-17 03:32:52 -0300

    fixing park migration

--- db/migrate/20180311022343_create_park.rb
+++ db/migrate/20180311022343_create_park.rb
@@ -1,9 +1,5 @@
 class CreatePark < ActiveRecord::Migration
-  def up
-  change_column :park, :some_column, :integer, after: :other_column
-	end
-  
-  def change
+	def change
 		create_table :parks do |t|
 			t.string :park_name
 			t.string :park_location
